84927ec18123710463cd4393eaaadb8779d8e920
SashaBagnovini99 <bsasha@student.ethz.ch>
2024-09-13 11:07:49 +0200

    Exercise 2

--- README.md
+++
@@ -1,3 +0,0 @@
-Hello, this is a simple text file.
-It contains vowels and consonants.
-Testing whether the code counts them correctly!
--- java2024/src/main/java/org/ethz/day_4/VowelConsonantCounter.java
+++ java2024/src/main/java/org/ethz/day_4/VowelConsonantCounter.java
@@ -1,6 +1,9 @@
 package org.ethz.day_4;
 import java.io.File;
-import java.util.*;
+import java.util.Scanner; 
+import java.io.FileNotFoundException;
+import java.util.Set; 
+
 
 public class VowelConsonantCounter {
     public static void main(String[] args) {
@@ -9,8 +12,17 @@ public class VowelConsonantCounter {
         Scanner input1 = new Scanner(System.in);
 
         String filepath = input1.nextLine();
+        input1.close();
+        
+        Scanner input;
         File file = new File(filepath);
-        Scanner input = new Scanner(file);
+        try {
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+            return;  // Exit the program if the file is not found
+        }
+        
 
 
         Set<Character> vowels = Set.of('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
@@ -20,14 +32,17 @@ public class VowelConsonantCounter {
         while (input.hasNext()) {
             String word = input.next();
             for (char ch : word.toCharArray()) {
-                if (vowels.contains(ch)) {
-                    vowelCount++;
-                } else {
-                    consonantCount++;
+                if (Character.isLetter(ch)) {
+                    if (vowels.contains(ch)) {
+                        vowelCount++;
+                    } else {
+                        consonantCount++;
+                    }
                 }
             }
         }
         System.out.println("The number of vowel is: " + vowelCount);
         System.out.println("The number of consonant is: " + consonantCount);
+        input.close();
     }
 }
